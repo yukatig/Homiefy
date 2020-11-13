d3d4f0e716d6bc51a5f85bf8010b8ace136bdc38
yukatig <yukati.g@gmail.com>
2020-11-12 18:52:17 -0800

    added credentials

--- web-api-auth-examples-master/authorization_code/app.js
+++ web-api-auth-examples-master/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '3628a923d3b94e21aa42b2f305e03b8c'; // Your client id
+var client_secret = '2d7c5941ea754a23ae050fed2b663189'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
