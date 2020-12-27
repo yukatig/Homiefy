6b5c9685b21687852ed6cf63f0f3d01d1843348a
yukatig <yukati.g@gmail.com>
2020-12-26 19:49:27 -0800

    lots of comments trying to print list of songs to console

--- web-api-auth-examples-master/authorization_code/app.js
+++ web-api-auth-examples-master/authorization_code/app.js
@@ -17,6 +17,8 @@ var client_id = '22c6082ec6974b168bb64b966f927bcf'; // Your client id
 var client_secret = 'a46390a2779f47bea29ff16b856f6152'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
+var global_access_token;
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -92,6 +94,9 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
+        console.log("hi");
+        global_access_token = access_token;
+
         var options = {
           url: 'https://api.spotify.com/v1/me',
           headers: { 'Authorization': 'Bearer ' + access_token },
@@ -110,6 +115,10 @@ app.get('/callback', function(req, res) {
             access_token: access_token,
             refresh_token: refresh_token
           }));
+
+        //RUN HELLO 
+        //hello();
+
       } else {
         res.redirect('/#' +
           querystring.stringify({
@@ -143,6 +152,79 @@ app.get('/refresh_token', function(req, res) {
     }
   });
 });
+
+
+/*
+var fetch = require('node-fetch');
+
+async function hello() {
+
+  var request = require('request');
+
+  var headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + global_access_token
+  };
+
+  var options = {
+      url: 'https://api.spotify.com/v1/me/player/recently-played',
+      headers: headers
+  };
+
+  function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log(body);
+      }
+
+      else {
+        console.log("not working");
+      }
+  }
+
+  request(options, callback);
+
+  //FETCH OPTION
+  var response = await fetch('https://api.spotify.com/v1/me/player/recently-played', {
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + global_access_token
+    }
+  });
+
+  const json = await response.json();
+
+  console.log(json); 
+  //FETCH OPTION END, WAS PRINTING TYPES OF ERROR 
+
+}
+
+hello(); */
+
+/*
+var request = require('request');
+
+var headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + global_access_token
+};
+
+var options = {
+    url: 'https://api.spotify.com/v1/me/player/recently-played',
+    headers: headers
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback); */
+
+
 
 console.log('Listening on 8888');
 app.listen(8888);
