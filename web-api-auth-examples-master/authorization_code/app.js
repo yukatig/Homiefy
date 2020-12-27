e3bbee53e3125e949e6926923c1ae5ac47b496e7
annieteaaa <51380406+annieteaaa@users.noreply.github.com>
2020-12-27 12:20:29 -0800

    dont override!!

--- web-api-auth-examples-master/authorization_code/app.js
+++ web-api-auth-examples-master/authorization_code/app.js
@@ -13,6 +13,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
+var global_access_token;
+
 var client_id = '22c6082ec6974b168bb64b966f927bcf'; // Your client id
 var client_secret = 'a46390a2779f47bea29ff16b856f6152'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
@@ -38,9 +40,56 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
+// View engine setup 
+app.set('view engine', 'ejs'); 
+
 app.use(express.static(__dirname + '/public'))
    .use(cors())
    .use(cookieParser());
+
+
+app.get('/play', function(req, res) {
+
+        var access_token = global_access_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me/player/recently-played?limit=10',
+          headers: { 'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + access_token }
+        };
+
+        request.get(options, function(error, response, body) {
+          
+          playdata = JSON.parse(body);
+          //console.log(playdata.items[0].track.album);
+
+          ourPlaylist = [];
+
+          var songs = playdata.items;
+
+          for(var i = 0; i < songs.length; i++) {
+            var artistlink = songs[i].track.album.artists[0].external_urls.spotify;
+            var artistname = songs[i].track.album.artists[0].name;
+            var songlink = songs[i].track.external_urls.spotify;
+            var image = songs[i].track.album.images[1].url;
+            var songname = songs[i].track.name;
+            ourPlaylist.push({
+              artref: artistlink,
+              artname: artistname,
+              songref: songlink,
+              name: songname,
+              cover: image
+            });
+          }
+
+          res.render('play', {
+            'playlist': ourPlaylist
+          });
+        });
+
+});
+
 
 app.get('/login', function(req, res) {
 
@@ -48,7 +97,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email user-read-recently-played';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -94,8 +143,7 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
-        console.log("hi");
-        global_access_token = access_token;
+            global_access_token = access_token;
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
@@ -224,7 +272,6 @@ function callback(error, response, body) {
 
 request(options, callback); */
 
-//merge
 
 
 console.log('Listening on 8888');
